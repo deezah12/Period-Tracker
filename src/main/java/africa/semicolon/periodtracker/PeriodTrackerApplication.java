e5331acb135fb40238a152f6c8fc9fd2f3c98c49
Hadiza <deezah30@gmail.com>
2023-04-24 00:24:17 +0100

    intial-commit

---
+++ src/main/java/africa/semicolon/periodtracker/PeriodTrackerApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.periodtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeriodTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/africa/semicolon/periodtracker/PeriodTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.periodtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
